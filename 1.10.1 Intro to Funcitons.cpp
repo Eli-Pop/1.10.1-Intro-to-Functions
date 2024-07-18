5d98dadad24e9e9057b732c7193108278e02ff0d
Elias V. <58016431+Eli-Pop@users.noreply.github.com>
2024-07-17 22:05:40 -0500

    Add files via upload

---
+++ 1.10.1 Intro to Funcitons.cpp
@@ -0,0 +1,18 @@
+// Use the abs () function.
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	int result;
+
+	result = abs(-10); 
+	//The calls the abs() function and assigns its return value to result.
+
+	cout << result << "\n";
+
+	system("pause");
+	return 0;
+}
